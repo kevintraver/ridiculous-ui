4426806f384770f1c5585c3ec3ab4b3056ab5856
Kevin Traver <kevintraver@kevintraver.com>
2025-03-21 14:51:04 -0700

    feat: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -83,11 +83,26 @@ const config: Config = {
           to: {
             height: '0'
           }
+        },
+        'shake': {
+          '0%, 100%': {
+            transform: 'translateX(0)'
+          },
+          '25%': {
+            transform: 'translateX(-5px)'
+          },
+          '50%': {
+            transform: 'translateX(5px)'
+          },
+          '75%': {
+            transform: 'translateX(-5px)'
+          }
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out'
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        'shake': 'shake 0.4s ease-in-out'
       }
     }
   },
